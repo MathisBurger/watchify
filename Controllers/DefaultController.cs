55717c6acab0e6d6605db000033b6d0dd50c0753
Mathis Burger <mburger@die-netzwerkstatt.de>
2022-05-04 09:54:52 +0200

    [TASK] updated default controller

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -1,16 +1,19 @@
 using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Formatters;
+using watchify.Filter;
 
 namespace watchify.Controllers;
 
 [Consumes(MediaTypeNames.Application.Json)]
 [Produces(MediaTypeNames.Application.Json)]
+[ProxyAddressFilter]
 [ApiController]
 public class DefaultController : ControllerBase
 {
-    
+
+
     [HttpGet("/")]
+    [RateLimitFilter]
     public ActionResult Default()
     {
         return Ok();
